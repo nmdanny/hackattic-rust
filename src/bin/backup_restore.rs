cc51fb587137524f6702da0a00966664b73871b9
NMDanny <nmdanny@gmail.com>
2018-01-13 02:28:58 +0200

    Finish backup_restore challenge

--- src/bin/backup_restore.rs
+++ src/bin/backup_restore.rs
@@ -5,13 +5,16 @@ extern crate serde_json;
 extern crate serde;
 #[macro_use]
 extern crate failure;
-extern crate diesel;
+extern crate postgres;
 extern crate tempfile;
+extern crate flate2;
 
 use hackattic::{HackatticChallenge, from_base64};
 use failure::{Error, ResultExt};
-use std::io::{Read, Write, BufWriter};
-use std::process::{Command, Stdio};
+use std::io::prelude::*;
+use std::process::{Command, Child, ChildStdin, ChildStdout, Stdio};
+use std::mem::ManuallyDrop;
+use postgres::{TlsMode, Connection};
 
 #[derive(Deserialize, Debug, Clone)]
 struct Problem {
@@ -28,21 +31,68 @@ fn main() {
     BackupRestore::process_challenge().unwrap();
 }
 
-fn main_err() -> Result<(), Error> {
-    BackupRestore::process_challenge()?;
-    Ok(())
+/// Wraps a `Connection` to a temporary database that
+/// will be automatically deleted once dropped.
+pub struct TempDb {
+    db_name: String,
+    connection: ManuallyDrop<Connection>
+}
+impl TempDb {
+    pub fn new(dump: &[u8]) -> Result<Self, Error> {
+        let dump = {
+            let mut decoder = flate2::read::GzDecoder::new(dump);
+            let mut buf = Vec::new();
+            decoder.read_to_end(&mut buf)
+                .context("It's possible that this dump wasn't encoded via GZip, try running this challenge again")?;
+            buf
+        };
+        let db_name = "hackattic_backup_restore_challenge".to_owned();
+        let create_db = Command::new("createdb")
+            .args(&["-U", "postgres", &db_name])
+            .env("PGPASSWORD", "postgres")
+            .status()?;
+        let psql = Command::new("psql")
+            .args(&["-U", "postgres", "-d", &db_name])
+            .env("PGPASSWORD", "postgres")
+            .stdin(Stdio::piped())
+            .spawn()?;
+        {
+            let mut stdin = psql.stdin.ok_or(format_err!("Failed to get stdin from new psql process"))?;
+            stdin.write_all(&dump)?;
+        }
+        let conn_string = format!("postgres://postgres:postgres@localhost:5432/{}", db_name);
+        let connection = Connection::connect(conn_string, TlsMode::None)?;
+        Ok(TempDb {
+            db_name, connection: ManuallyDrop::new(connection)
+        })
+    }
+    fn find_ssns(&mut self) -> Result<Vec<String>, Error> {
+        let results = self.query("SELECT ssn FROM criminal_records where status = 'alive'", &[])?
+            .iter()
+            .map(|row| row.get(0))
+            .collect();
+        Ok(results)
+    }
+}
+impl Drop for TempDb {
+    fn drop(&mut self) {
+        unsafe {
+            // we must drop the connection before we can drop the database
+            std::mem::ManuallyDrop::drop(&mut self.connection);
+        }
+        Command::new("dropdb")
+            .args(&["-U", "postgres", &self.db_name])
+            .env("PGPASSWORD", "postgres")
+            .status().unwrap();
+
+    }
 }
 
-fn load_db(dump: &[u8]) -> Result<(), Error> {
-    let mut file = ::tempfile::NamedTempFile::new()?;
-    file.write_all(dump);
-    let file_path = file.path().to_str().unwrap();
-    println!("pg_restore -C -d hackattic_dump -d postgres {}", file_path);
-    let cmd = Command::new("pg_restore")
-        .args(&["-C", "-d hackattic_dump", "-d postgres", file_path])
-        .output()?;
-    Ok(())
-
+impl ::std::ops::Deref for TempDb {
+    type Target = Connection;
+    fn deref(&self) -> &Connection {
+        self.connection.deref()
+    }
 }
 
 struct BackupRestore;
@@ -53,9 +103,10 @@ impl HackatticChallenge for BackupRestore {
         "backup_restore"
     }
     fn make_solution(req: &Problem) -> Result<Solution, Error> {
-        load_db(&req.dump)?;
+        let mut db = TempDb::new(&req.dump)?;
+        let ssns = db.find_ssns()?;
         Ok(Solution {
-            alive_ssns: vec![]
+            alive_ssns: ssns
         })
     }
 }

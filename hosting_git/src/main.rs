19669064bb5801725c5b6b7c0e0758ebd47260ed
NMDanny <nmdanny@gmail.com>
2018-03-23 12:19:13 +0300

    cmd handling should be more robust now

--- hosting_git/src/main.rs
+++ hosting_git/src/main.rs
@@ -22,12 +22,13 @@ use std::process::{Command, Output};
 use std::io::{Write, Read};
 use hackattic_common::{HackatticChallenge, make_reqwest_client};
 use failure::{Error, ResultExt};
+use std::path::PathBuf;
 
 #[derive(Debug, Deserialize)]
 struct Problem {
     push_token: String,
     username: String,
-    repo_path: String,
+    repo_path: PathBuf,
     ssh_key: String
 }
 
@@ -47,51 +48,76 @@ fn push_message(push_token: &str, repo_host: &str) -> Result<reqwest::Response, 
         Ok(res)
 }
 
-#[cfg(target_os = "unix")]
+
+fn su_as(cmd: duct::Expression, user: &str) -> duct::Expression {
+    cmd.pipe(cmd!("su", user))
+}
+fn sudo_as(cmd: duct::Expression, user: &str) -> duct::Expression {
+    cmd.pipe(cmd!("sudo", "-u", user, "sh"))
+}
+
+//#[cfg(target_os = "unix")]
 fn setup_git_server(problem: &Problem) -> Result<(), Error> {
-    // BUG: command injection possible wherever format! is used
 
-    info!("Creating user \"{}\"", problem.username);
-    cmd!("adduser", problem.username, "--disabled-password").run()?;
+    info!("Creating user \"{}\"", &problem.username);
+    sudo_as(cmd!("adduser", "--disabled-password", "--gecos", "''", &problem.username), "root").run()?;
 
-    info!("Adding specified ssh-key to authorized_keys");
-    cmd!("su", "-c", "mkdir .ssh && chmod 700 .ssh", problem.username).run()?;
-    cmd!("su", "-c", "touch .ssh/authorized_keys && chmod 600 .ssh/authorized_keys", problem.username).run()?;
-    cmd!("su", "-c", format!("echo {} >> .ssh/authorized_keys", problem.ssh_key), problem.username).run()?;
+    info!("Adding specified ssh-key to the newly adder user's authorized_keys");
+    su_as(cmd!("mkdir ~/.ssh && chmod 700 ~/.ssh"), &problem.username).run()?;
+    su_as(cmd!("touch ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys"), &problem.username).run()?;
+    su_as(cmd!("cat").input(problem.ssh_key.as_bytes()).pipe(cmd!("tee", "-a", "~/.ssh/authorized_keys")), &problem.username).run()?;
 
-    let repo_folder = problem.repo_path.split("/").next().unwrap();
-    info!("Initializing git directory structure at the following folder: {}", repo_folder);
-    cmd!("su", "-c", format!("mkdir {} && cd {} && git init --bare", repo_folder, repo_folder), problem.username).run()?;
+
+    let repo_path = format!("~/{:?}", &problem.repo_path);
+    info!("Initializing git directory structure at the following folder: {}", repo_path);
+    su_as(cmd!("mkdir", "-p", &repo_path).then(
+          cmd!("cd", &repo_path)).then(
+          cmd!("git", "init", "--bare")),
+          &problem.username).run()?;
 
     
-    cmd!("chsh", problem.username, "-s", "$(which git-shell)").run()?;
+    sudo_as(cmd!("chsh", &problem.username, "-s", "$(which git-shell)"), "root").run()?;
 
     info!("(Re)starting ssh-server");
-    cmd!("service", "ssh", "restart");
+    sudo_as(cmd!("service", "ssh", "restart"), "root").run()?;
     info!("git-server ready!!");
 
     Ok(())
 }
 
+/*
 #[cfg(not(target_os = "unix"))]
 fn setup_git_server(problem: &Problem) -> Result<(), Error> {
     panic!("setup_git_server currently only works on *nix systems");
 }
+*/
 
 #[derive(Debug, Serialize)]
 struct Solution {
     secret: String
 }
 
+fn dummy_run() {
+    let problem = Problem {
+        push_token: "nevermind".to_owned(),
+        username: "somebody".to_owned(),
+        repo_path: "who/cares.git".into(),
+        // pkey of nmdanny on wsl
+        ssh_key: "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDARUm67vu1ibLIV0yoF/ObZvHyNbviuFCFnlvaukmUIjL1067T2CX5/+rtPhj19l+pKeKJ/t+PYB9tBuRaMv+cfJEMDlJkOshTVsciev85ncUSODy/+2SQelN34+GsFlgzOmVcNE5LMSPId78IeiHkh/BOB/bKo68PXpZLOJncvk5/LjUWXz7E/n460NbEZhIvuCzBcmcAcE8o/sU8plWReSOrbykPTb7jk4+xpJDO0TEIoj7QemNRU8Ms8ruAukE7bg349si45XcZoJ0adtQnpFvxm9/LglkrTecCsp1taEqn5Owwnw75eOdM9MmyYZp99ljMdzcCTpSq5OcF/0eJ nmdanny@DESKTOP-7VLKOF6".to_owned()
+    };
+    setup_git_server(&problem).unwrap();
+}
 
 fn main() {
-    ansi_term::enable_ansi_support().unwrap();
+    #[cfg(target_os = "windows")] {
+        ansi_term::enable_ansi_support();
+    }
     let mut builder = pretty_env_logger::formatted_builder().unwrap();
-    builder.filter(Some("git_server"), log::LevelFilter::Debug);
+    builder.filter(Some("hosting_git"), log::LevelFilter::Debug);
     builder.init();
     info!("Logger initialized");
-    HostingGit::process_challenge().unwrap();
-    info!("Challenge processed.");
+    dummy_run();
+    //HostingGit::process_challenge().unwrap();
 }
 
 struct HostingGit;
@@ -100,6 +126,7 @@ impl HackatticChallenge for HostingGit {
     type Solution = Solution;
 
     fn make_solution(problem: &Self::Problem) -> Result<Self::Solution, Error> {
+        info!("Processing problem:\n{:?}", problem);
         setup_git_server(problem)?;
         push_message(&problem.push_token, "nmdanny.ddns.net")?;
         let mut rl = rustyline::Editor::<()>::new();
